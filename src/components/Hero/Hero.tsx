d38c4a83a961386d56e2b025c4452a99f7d7570e
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-10 15:16:53 -0300

    feat: img alt added

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,10 +11,10 @@ export const Hero = () => {
                     <p><span>Designer</span>: Transformo ideias em interfaces visuais atraentes e funcionais.</p>
                     <p><span>Social Media</span>: Aumento presença online e engajamento com estratégias criativas</p>
                     <p><span>Edição de Vídeos</span>: Produzo e edito vídeos envolventes com um serviço rápido e de qualidade.</p>
-                    <button><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" />Contato</a></button>
+                    <button><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" alt='Contato' />Contato</a></button>
                 </div>
                 <div className='hero-right'>
-                    <img src='/imgs/code-hero.png' />
+                    <img src='/imgs/code-hero.png' alt='Brendon Silva Informações' />
                 </div>
             </section>
         </>
