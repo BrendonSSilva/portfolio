ae33db81b9867fa62400b6464135cba1e824d75f
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-10 17:04:02 -0300

    chore: button styles added

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,7 +11,7 @@ export const Hero = () => {
                     <p><span>Designer</span>: Transformo ideias em interfaces visuais atraentes e funcionais.</p>
                     <p><span>Social Media</span>: Aumento presença online e engajamento com estratégias criativas</p>
                     <p><span>Edição de Vídeos</span>: Produzo e edito vídeos envolventes com um serviço rápido e de qualidade.</p>
-                    <button><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" alt='Contato' />Contato</a></button>
+                    <button className="button-default"><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" alt='Contato' />Contato</a></button>
                 </div>
                 <div className='hero-right'>
                     <img src='/imgs/code-hero.png' alt='Brendon Silva Informações' />
