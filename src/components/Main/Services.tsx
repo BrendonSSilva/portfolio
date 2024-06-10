8a228705fa8751a922aaf98143406f33df8ccf02
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-10 10:19:30 -0300

    feat: services section added

---
+++ src/components/Main/Services.tsx
@@ -0,0 +1,14 @@
+import "./main.scss";
+import React from 'react';
+export const Services = () => {
+
+    return (
+        <>
+            <section className="services">
+                <div>
+                    <img src='/imgs/www.png' />
+                </div>
+            </section>
+        </>
+    );
+};
