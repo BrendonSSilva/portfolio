37db1bc3148189abe5b17bd48973f08520b703be
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-10 10:19:44 -0300

    feat: hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { ThemeProvider } from 'styled-components';
 import { theme } from './assets/styles/index'
 import { Navbar } from './components/Navbar/Navbar';
+import { Hero } from './components/Hero/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <ThemeProvider theme={theme}>
       </ThemeProvider>
     </>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import "./hero.scss";
+import React from 'react';
+export const Hero = () => {
+
+    return (
+        <>
+            <section className="hero">
+                <div className='hero-left'>
+                    <h1>Brendon S. da Silva</h1>
+                    <p><span>Desenvolvedor</span>: Crio soluções inovadoras com diversas linguagens e frameworks modernos.</p>
+                    <p><span>Designer</span>: Transformo ideias em interfaces visuais atraentes e funcionais.</p>
+                    <p><span>Social Media</span>: Aumento presença online e engajamento com estratégias criativas</p>
+                    <p><span>Edição de Vídeos</span>: Produzo e edito vídeos envolventes com um serviço rápido e de qualidade.</p>
+                    <button><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" />Contato</a></button>
+                </div>
+                <div className='hero-right'>
+                    <img src='/imgs/code-hero.png' />
+                </div>
+            </section>
+        </>
+    );
+};
