bf78437ce83836d06e39f15af0a218d099b261af
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-10 15:13:24 -0300

    feat: services section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './assets/styles/index'
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
+import { Services } from './components/Main/Services';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Services />
       <ThemeProvider theme={theme}>
       </ThemeProvider>
     </>
