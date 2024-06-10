8a296f0884966066cc4ce4da3c7ca7608cb15d55
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-10 09:33:45 -0300

    feat: navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { ThemeProvider } from 'styled-components';
 import { theme } from './assets/styles/index'
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <ThemeProvider theme={theme}>
-        
       </ThemeProvider>
     </>
   )
