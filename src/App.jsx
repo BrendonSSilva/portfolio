4551a1517d202db24351222dcb1f53b59ea7d452
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-12 20:55:26 -0300

    feat: router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
 import { Services } from './components/Main/Services';
@@ -6,17 +7,28 @@ import { Skills } from './components/Main/Skills';
 import { Card1 } from './components/Card1/Card1';
 import { Courses } from './components/Main/Courses';
 import { Footer } from './components/Footer/Footer';
+import { Jobs } from './components/Jobs/Jobs';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Services />
-      <Skills />
-      <Card1 />
-      <Courses/>
-      <Footer/>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Navbar />
+              <Hero />
+              <Services />
+              <Skills />
+              <Jobs />
+              <Courses />
+              <Footer />
+            </>
+          } />
+          <Route path="/trabalhos" element={<Card1 route='' />} />
+        </Routes>
+      </Router>
+
       {/* <ThemeProvider theme={theme}>
       </ThemeProvider> */}
     </>
