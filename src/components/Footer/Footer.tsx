23ba2b0385c072e16eadcefbee8d40d2bd6ca38b
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-12 19:31:12 -0300

    feat: footer added

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import "./styles.scss";
+import React from 'react';
+
+export const Footer = () => {
+
+    return (
+        <>
+            <footer>
+                <button className='button-default' style={{ marginBottom: '30px' }}><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" alt='Contato' />Contato</a></button>
+                <div className='languages'>
+                    <h2>Idiomas</h2>
+                    <p><span>Inglês</span> - Intermediário para conversação, escrita e leitura.</p>
+                    <p><span>Portugês</span> - Nativo.</p>
+                </div>
+                <div className='social'>
+                    <h2>Contato e Redes Sociais</h2>
+                    <a href="https://www.linkedin.com/in/brendon-silva/"><img src='/icons/linkedin.svg' alt='LinkedIn' /></a>
+                    <a href="https://github.com/BrendonSSilva"><img src='/icons/github.svg' alt='GitHub' /></a>
+                    <a href="https://github.com/BrendonSSilva"><img src='/icons/Instagram.svg' alt='Instagram' /></a>
+                    <a href="https://github.com/BrendonSSilva"><img src='/icons/discord.svg' alt='Discord' /></a>
+                    <a href="https://github.com/BrendonSSilva"><img src='/icons/wpp.svg' alt='WhatsApp' /></a>
+                    <a href="https://github.com/BrendonSSilva"><img src='/icons/Gmail.svg' alt='Gmail' /></a>
+                </div>
+            <hr />
+            <p>Brendon © 2024</p>
+            </footer>
+        </>
+    );
+};
