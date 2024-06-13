e9ecff5b219c7ab97eac8282feaaeea6b1a9f0c9
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-12 21:22:18 -0300

    feat: SEO updates

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -12,10 +12,10 @@ export const Hero = () => {
                     <p><span>Designer</span>: Transformo ideias em interfaces visuais atraentes e funcionais.</p>
                     <p><span>Social Media</span>: Aumento presença online e engajamento com estratégias criativas</p>
                     <p><span>Edição de Vídeos</span>: Produzo e edito vídeos envolventes com um serviço rápido e de qualidade.</p>
-                    <button className="button-default"><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" alt='Contato' />Contato</a></button>
+                    <button className="button-default"><a href="https://www.linkedin.com/in/brendon-silva/"><img src="/icons/cta-icon.svg" alt='Contato' width="20" height="20" loading="lazy"/>Contato</a></button>
                 </div>
                 <div className='hero-right'>
-                    <img src='/imgs/code-hero.png' alt='Brendon Silva Informações' />
+                    <img src='/imgs/code-hero.webp' alt='Brendon Silva Informações' />
                 </div>
             </section>
         </>
--- src/components/Main/Courses.tsx
+++ src/components/Main/Courses.tsx
@@ -9,7 +9,7 @@ export const Courses = () => {
             <section className="courses">
                 <div className="courses-left">
                     <div className="course-item obc">
-                        <img src="/imgs/obc.png" alt="OneBitCode" />
+                        <img src="/imgs/obc.webp" alt="OneBitCode" />
                         <div>
                             <h3>OneBitCode</h3>
                             <p>Desenvolvimento Back-End | 11/23 - 01/24 - <a href="https://cursos.onebitcode.com/cert/99J0SGRB5rHw8qc" target="_blank">Exibir Credencial</a></p>
@@ -22,7 +22,7 @@ export const Courses = () => {
                         </div>
                     </div>
                     <div className="course-item b7web">
-                        <img src="/imgs/b7web.png" alt="B7Web" />
+                        <img src="/imgs/b7web.webp" alt="B7Web" />
                         <div>
                             <h3>B7Web</h3>
                             <p>JavaScript | 01/22 - 05/22 - <a href="https://alunos.b7web.com.br/media/certificates/certificado_4512946.jpg" target="_blank">Exibir Credencial</a></p>
@@ -32,7 +32,7 @@ export const Courses = () => {
                 <div className="courses-right">
                     <h1>Formação e Cursos</h1>
                     <p>Optei por cursos profissionalizantes e projetos próprios, seguindo conselhos valiosos de profissionais experientes e grandes empresas. Essa abordagem prática e focada me permitiu adquirir habilidades relevantes e aplicáveis no mercado, aprimorando continuamente meu conhecimento e competências.</p>
-                    <img src="/imgs/joaofrango.png" alt="Formação" />
+                    <img src="/imgs/joaofrango.webp" alt="Formação" />
                 </div>
             </section>
             <hr />
