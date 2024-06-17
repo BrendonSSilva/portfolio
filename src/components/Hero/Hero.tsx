de4143f74687de6ad5b9e0cddec212a3769135ff
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-17 18:34:26 -0300

    feat: obs modified

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
         <>
             <section className="hero">
                 <div className='hero-left'>
-                    <p style={{marginTop: '50px'}}><span>Projeto em Desenvolvimento**</span></p>
+                    {/* <p style={{marginTop: '50px'}}><span>Projeto em Desenvolvimento**</span></p> */}
                     <h1>Brendon S. da Silva</h1>
                     <p><span>Desenvolvedor</span>: Crio soluções inovadoras com diversas linguagens e frameworks modernos.</p>
                     <p><span>Designer</span>: Transformo ideias em interfaces visuais atraentes e funcionais.</p>
