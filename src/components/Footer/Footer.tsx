7b8d0f13d26c0b20880f510d722557974ef39836
Brendon Silva <brendon.dasilva03@gmail.com>
2024-06-17 18:32:34 -0300

    chore: links and discord removed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,13 +16,12 @@ export const Footer = () => {
                     <h2>Contato e Redes Sociais</h2>
                     <a href="https://www.linkedin.com/in/brendon-silva/"><img src='/icons/linkedin.svg' alt='LinkedIn' /></a>
                     <a href="https://github.com/BrendonSSilva"><img src='/icons/github.svg' alt='GitHub' /></a>
-                    <a href="https://github.com/BrendonSSilva"><img src='/icons/Instagram.svg' alt='Instagram' /></a>
-                    <a href="https://github.com/BrendonSSilva"><img src='/icons/discord.svg' alt='Discord' /></a>
-                    <a href="https://github.com/BrendonSSilva"><img src='/icons/wpp.svg' alt='WhatsApp' /></a>
-                    <a href="https://github.com/BrendonSSilva"><img src='/icons/Gmail.svg' alt='Gmail' /></a>
+                    <a href="https://www.instagram.com/brendonsilva03"><img src='/icons/Instagram.svg' alt='Instagram' /></a>
+                    <a href="https://wa.me/5553984070032"><img src='/icons/wpp.svg' alt='WhatsApp' /></a>
+                    <a href="mailto:brendon.dasilva03@gmail.com"><img src='/icons/Gmail.svg' alt='Gmail' /></a>
                 </div>
-            <hr />
-            <p>Brendon © 2024</p>
+                <hr />
+                <p>Brendon © 2024</p>
             </footer>
         </>
     );
